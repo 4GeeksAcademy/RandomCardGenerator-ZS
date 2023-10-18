e503862d315d51d22f695ac00b61e078eb774218
zetaese0 <141279892+zetaese0@users.noreply.github.com>
2023-10-18 19:38:04 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,52 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#iconosup").innerHTML = generadoricono();
+  document.querySelector("#icono").innerHTML = document.querySelector(
+    "#iconosup"
+  ).innerHTML;
+  document.querySelector("#valor").innerHTML = generadorvalor();
+};
+
+//write your code here
+
+let generadoricono = () => {
+  let suits = [
+    '<span style="color: #000">♠</span><br />',
+    '<span style="color: #f10d0d">♥</span><br />',
+    '<span style="color: #f10d0d">♦</span><br />',
+    '<span style="color: #000">♣</span><br />'
+  ];
+  let palo = suits[Math.floor(Math.random() * suits.length)];
+
   console.log("Hello Rigo from the console!");
+  console.log(palo);
+
+  return palo;
+
+  // estoy pensando en poner un objeto que tenga 2 clases : palos y valores y que al recargar la pagina sea un random de cada
+};
+
+let generadorvalor = () => {
+  let valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let carta = valores[Math.floor(Math.random() * valores.length)];
+
+  return carta;
+
+  // estoy pensando en poner un objeto que tenga 2 clases : palos y valores y que al recargar la pagina sea un random de cada
 };
